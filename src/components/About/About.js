8bb4b81c7f3ba57f4171ed9040f2ab842a3d8538
Sharifa Simon <sharifa.a.simon@gmail.com>
2020-10-29 10:48:18 -0500

    Add basic routing with react-router-dom. Supply components with basic functionality and css files.

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+
+function About() {
+    return (
+      
+      <div className="About">
+        This is the About view.
+      
+      </div>
+      
+    );
+  }
+  
+  export default About;
---
+++ src/components/Map/Map.js
@@ -0,0 +1,14 @@
+import './Map.css';
+
+function Map() {
+    return (
+      
+      <div className="Maps">
+        This is the Map view.
+      
+      </div>
+      
+    );
+  }
+  
+  export default Map;
