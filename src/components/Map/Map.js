1ef6d8c4066e28ffb3713bc3dec09d1baec70be9
Sharifa Simon <sharifa.a.simon@gmail.com>
2020-11-02 23:30:24 -0600

    make map change from dropdown menu selections.

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,101 +5,168 @@ import { VectorMap } from "react-jvectormap";
 class Map extends Component {
 
   state = {
-
   }
 
-  componentDidUpdate() {
-
+  handleDropdown = (event) => {
+    // when a country is selected in the dropdown menu, it provides the state with values which
+    // then highlight countries in the map, on the DOM, based on user's selection
+    //TO-DO: make this process more efficient - can we reduce number of functions used? method?
+    switch (event.target.value) {
+      case 'Canada':
+        this.setState({
+          CA: 1,
+          US: 2,
+          MX: '',
+          BZ: '',
+          GT: '',
+          SV: '',
+          HN: '',
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'States':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: '',
+          BZ: '',
+          GT: '',
+          SV: '',
+          HN: '',
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'Mexico':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: '',
+          GT: '',
+          SV: '',
+          HN: '',
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'Belize':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: 4,
+          GT: '',
+          SV: '',
+          HN: '',
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'Guatemala':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: '',
+          GT: 5,
+          SV: '',
+          HN: '',
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'Salvador':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: '',
+          GT: 5,
+          SV: 6,
+          HN: '',
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'Honduras':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: '',
+          GT: 5,
+          SV: '',
+          HN: 7,
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'Nicaragua':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: '',
+          GT: 5,
+          SV: '',
+          HN: 7,
+          NI: 8,
+          CR: '',
+          PA: ''
+        });
+        break;
+      case 'Costa':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: '',
+          GT: 5,
+          SV: '',
+          HN: 7,
+          NI: 8,
+          CR: 9,
+          PA: ''
+        });
+        break;
+      case 'Panama':
+        this.setState({
+          CA: '',
+          US: 2,
+          MX: 3,
+          BZ: '',
+          GT: 5,
+          SV: '',
+          HN: 7,
+          NI: 8,
+          CR: 9,
+          PA: 10
+        });
+        break;
+      default:
+        this.setState({
+          CA: '',
+          US: '',
+          MX: '',
+          BZ: '',
+          GT: '',
+          SV: '',
+          HN: '',
+          NI: '',
+          CR: '',
+          PA: ''
+        });
+    }
   }
-
-
-  addCanada = (event) => {
-    console.log('Clicking Canada Button!', event.target.value)
-    this.setState({
-      CA: 1,
-      US: '',
-      MX: '',
-      BZ: '',
-      GT: '',
-      SV: '',
-      HN: '',
-      NI: '',
-      CR: '',
-      PA: ''
-    });
-    console.log('Testing state', this.state)
-  }
-
-  addUSA = (event) => {
-    console.log('Clicking USA Button!', event.target.value)
-    this.setState({
-      ...this.state,
-      CA: 1,
-      US: 2,
-      MX: '',
-      BZ: '',
-      GT: '',
-      SV: '',
-      HN: '',
-      NI: '',
-      CR: '',
-      PA: ''
-    });
-    console.log('Testing state', this.state)
-  }
-
-  addMexico = (event) => {
-    console.log('Clicking Mexico Button!', event.target.value)
-    this.setState({
-      CA: 1,
-      US: 2,
-      MX: 3,
-      BZ: '',
-      GT: '',
-      SV: '',
-      HN: '',
-      NI: '',
-      CR: '',
-      PA: ''
-    });
-    console.log('Testing state', this.state)
-  }
-
-  addBelize = (event) => {
-    console.log('Clicking Belize Button!', event.target.value)
-    this.setState({
-      CA: 1,
-      US: 2,
-      MX: 3,
-      BZ: 4,
-      GT: '',
-      SV: '',
-      HN: '',
-      NI: '',
-      CR: '',
-      PA: ''
-    });
-    console.log('Testing state', this.state)
-  }
-
-  addGuatemala = (event) => {
-    console.log('Clicking Guatemala Button!', event.target.value)
-    this.setState({
-      CA: 1,
-      US: 2,
-      MX: 3,
-      BZ: 4,
-      GT: 5,
-      SV: '',
-      HN: '',
-      NI: '',
-      CR: '',
-      PA: ''
-    });
-    console.log('Testing state', this.state)
-  }
-
-
 
 
   render() {
@@ -117,27 +184,53 @@ class Map extends Component {
     };
 
     return (
-      <div className="Maps">
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
-        Select a country to see where a shipment must travel before arriving at it's destination:
-        <br /> <button value='1' onClick={this.addCanada}>Canada</button> <span />
-        <button value='2' onClick={this.addUSA}>United States</button> <span />
-        <button value='3' onClick={this.addMexico}>Mexico</button> <span />
-        <button value='4' onClick={this.addBelize}>Belize</button> <span />
-        <button value='5' onClick={this.addGuatemala}>Guatemala</button>
+      <div className="container">
+        <div className="mapButtons">
+          {/* <pre>{JSON.stringify(this.state, null, 2)}</pre> */}
+       
+          <label for="selectCountries"> Select a country to see where a shipment must travel before arriving at it's destination:</label>
 
-        <div style={{ width: 500, height: 500 }}>
+          <select name="selectCountries" id="selectCountries" onChange={this.handleDropdown}>
+            <option value="">None</option>
+            <option value="Canada" >Canada</option>
+            <option value="States">United States</option>
+            <option value="Mexico">Mexico</option>
+            <option value="Belize">Belize</option>
+            <option value="Guatemala">Guatemala</option>
+            <option value="Salvador">El Salvador</option>
+            <option value="Honduras">Honduras</option>
+            <option value="Nicaragua">Nicaragua</option>
+            <option value="Costa">Costa Rica</option>
+            <option value="Panama">Panama</option>
+          </select>
+        </div>
+        <div className="northAmericaMap" style={{ width: 500, height: 500 }}>
           <VectorMap map={'north_america_mill'}
-            backgroundColor="#3b96ce"
+            backgroundColor="#bfd3e6" // color of the "water"
             ref="map"
             zoomOnScroll={false}
             regionsSelectable={false}
             containerStyle={{
-              width: '150%',
-              height: '150%'
+              width: '100%',
+              height: '100%'
+            }}
+            regionStyle={{
+              initial: {
+                fill: "#8c6bb1", // color for the default map
+              },
+              selected: {
+                fill: "#4d004b" // color of selected country
+              },
             }}
             selectedRegions={this.state}
             containerClassName="map"
+            focusOn={{ // loads the map to a more specific, zoomed in area
+              x: 0.5,
+              y: 1,
+              scale: 1.5,
+              animate: false
+            }}
+            
           //markers={this.state.US}
 
 
