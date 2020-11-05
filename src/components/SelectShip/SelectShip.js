d25d63ec0a5c00a5989947f6dbd9915fc6932f85
Sharifa Simon <sharifa.a.simon@gmail.com>
2020-11-05 00:26:14 -0600

    Create shipment selection on home screen. Set link to map. Test styling. Add images to home.

--- src/components/SelectShip/SelectShip.js
+++ src/components/SelectShip/SelectShip.js
@@ -1,14 +1,74 @@
 import './SelectShip.css';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import DirectionsBoatIcon from '@material-ui/icons/DirectionsBoat';
+import Button from 'react-bootstrap/Button';
 
 function SelectShip() {
     return (
-      
-      <div className="Ship">
-        This is the SelectShip view.
-      
-      </div>
-      
+
+        <Container className="shipContainer">
+            <div className="title">Select your form of shipment:</div>
+            <CardDeck>
+                <Card>
+
+                    <Card.Body>
+                        <Card.Title>AIR FREIGHT</Card.Title>
+                        <Container className="shipImages">
+                            <FlightTakeoffIcon style={{ fontSize: 80 }} />
+                        </Container>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button variant="secondary" size="sm" disabled>
+                            Get Started
+                        </Button>
+                    </Card.Footer>
+                </Card>
+
+                <Card>
+                    <Card.Body>
+                        <Card.Title>TRUCKLOAD</Card.Title>
+                        <Container className="shipImages">
+                        <LocalShippingIcon style={{ fontSize: 80 }} />
+                        </Container>
+                        <Card.Text>
+
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button href="/map" variant="secondary" size="sm">
+                            Get Started
+                        </Button>
+                    </Card.Footer>
+                </Card>
+
+                <Card>
+                    <Card.Body>
+                        <Card.Title>OCEAN SHIPPING</Card.Title>
+                        <Container className="shipImages">
+                        <DirectionsBoatIcon style={{ fontSize: 80 }} />
+                        </Container>
+                        <Card.Text>
+                            Coming Soon
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button variant="secondary" size="sm" disabled>
+                            Get Started
+                        </Button>
+                    </Card.Footer>
+                </Card>
+            </CardDeck>
+        </Container>
+        //   <div className="containerShip">
+        //     This is the SelectShip view.
+
+        //   </div>
+
     );
-  }
-  
-  export default SelectShip;
+}
+
+export default SelectShip;
